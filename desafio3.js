d6800e962c3a960a436ce7563b06836ee61457af
vivi-toto <fernandavitoria856@gmail.com>
2023-10-08 16:50:36 -0400

    adicionados objetos

--- desafio3.js
+++ desafio3.js
@@ -5,3 +5,15 @@ class heroi {
         this.tipo = tipo;
     }
 }
+
+let guerreiro = new heroi("Ragnar", 28, "guerreiro"); 
+let mago = new heroi("Alaric", 35, "mago");
+let monge = new heroi("Balbornas", 47, "monge");
+let ninja = new heroi("Hatori", 32, "ninja");
+
+console.log(guerreiro);
+console.log(mago);
+console.log(monge);
+console.log(ninja);
+
+
