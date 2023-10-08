49a8404b466fc70b3d26c8e601e2ff720d09c578
vivi-toto <fernandavitoria856@gmail.com>
2023-10-08 17:00:20 -0400

    adicionado metodo atacar

--- desafio3.js
+++ desafio3.js
@@ -1,8 +1,13 @@
 class heroi {
-    constructor(nome, idade, tipo) {
+    constructor(nome, idade, tipo, ataque) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
+        this.ataque = ataque;
+    }
+
+    atacar(){
+        console.log(`O tipo ${this,tipo} atacou usando ${this.ataque}`)
     }
 }
 
@@ -11,9 +16,11 @@ let mago = new heroi("Alaric", 35, "mago");
 let monge = new heroi("Balbornas", 47, "monge");
 let ninja = new heroi("Hatori", 32, "ninja");
 
-console.log(guerreiro);
-console.log(mago);
-console.log(monge);
-console.log(ninja);
+// console.log(guerreiro);
+// console.log(mago);
+// console.log(monge);
+// console.log(ninja);
+
+
 
 
